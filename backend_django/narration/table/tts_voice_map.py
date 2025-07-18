77693621afff100280adaee733bb95b67999b105
daiseek <dnjsgml0578@gmail.com>
2025-07-19 00:00:43 +0900

    [fix] 보이스 매핑 테이블 수정

--- backend_django/narration/table/tts_voice_map.py
+++ backend_django/narration/table/tts_voice_map.py
@@ -3,12 +3,15 @@
 # 서버가 이 테이블을 사용해 voice id 선택
 VOICE_SELECTION_TABLE = {
     # 여성
-    ("여성", "teen"): "ZJCNdZEjYwkOElxugmW2",      # Bella
-    ("여성", "adult"): "ZJCNdZEjYwkOElxugmW2",     # Rachel
-    ("여성", "senior"): "ZJCNdZEjYwkOElxugmW2",    # Rachel (대체용)
+    ("여성", "teen"): "sSoVF9lUgTGJz0Xz3J9y",      # 
+    ("여성", "adult"): "ksaI0TCD9BstzEzlxj4q",     # 
+    ("여성", "senior"): "8MwPLtBplylvbrksiBOC",    # 
+    ("여성", "else"): "CnnL9KfZBW3JEUs1JPeS",  # 동물
 
     # 남성
-    ("남성", "teen"): "ZJCNdZEjYwkOElxugmW2",      # Domi (중성 느낌)
-    ("남성", "adult"): "ZJCNdZEjYwkOElxugmW2",     # Clyde
-    ("남성", "senior"): "ZJCNdZEjYwkOElxugmW2",    # Arnold
+    ("남성", "teen"): "4JJwo477JUAx3HV0T7n7",      # 
+    ("남성", "adult"): "ZJCNdZEjYwkOElxugmW2",   # 
+    ("남성", "senior"): "5ON5Fnz24cnOozEQfGAm",    # 
+    ("남성", "else"): "4JJwo477JUAx3HV0T7n7",  # 
+
 }
