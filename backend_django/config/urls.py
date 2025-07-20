37a15c5bcd87030ac6da158c0e409f9cee8af51d
daiseek <dnjsgml0578@gmail.com>
2025-07-20 23:27:21 +0900

    docs: swagger 관련 url 설정

--- backend_django/config/urls.py
+++ backend_django/config/urls.py
@@ -18,24 +18,48 @@ from django.contrib import admin
 from django.urls import path, include
 from django_prometheus import exports
 from config.views import index
+from django.conf import settings
+
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("metrics", exports.ExportToDjangoView),
 
-    path('books/', include('books.urls')),  # Books 애플리케이션의 URL 포함
-
-    path('videos/', include('videos.urls')),  # Videos 애플리케이션의 URL 포함
+    path('books/', include('books.urls')),
+    path('videos/', include('videos.urls')),
     path('videos2/', include('videos2.urls')),
     path('voe3Video/', include('voe3Video.urls')),
-
-    path('characters/', include('characters.urls')),  # Characters 애플리케이션의 URL 포함
-    path('users/', include('users.urls')),  # Users 애플리케이션의 URL 포함
-    path("", index), # 루트 페이지 요청하면, index 함수를 호출하라는 의미
-
-    path('narration/', include('narration.urls')),  # Narration 애플리케이션의 URL 포함
-
-    # path('s3test/', include('s3test.urls')),  # S3 테스트용 앱의 URL 포함
-    path('', include('django_prometheus.urls'))
-
+    path('characters/', include('characters.urls')),
+    path('users/', include('users.urls')),
+    path("", index),
+    path('narration/', include('narration.urls')),
+    path('', include('django_prometheus.urls')),
 ]
+
+'''DEBUG=TRUE, 개발환경일때만 Swagger UI를 사용'''
+if settings.DEBUG:
+    from drf_yasg.views import get_schema_view
+    from drf_yasg import openapi
+    from rest_framework import permissions
+    from django.conf.urls.static import static
+
+    # Swagger 설정
+    schema_view = get_schema_view(
+        openapi.Info(
+            title="EpiLog API",
+            default_version='v1',
+            description="API documentation for your project",
+        ),
+        public=True,
+        permission_classes=[permissions.AllowAny],
+        url='http://localhost:28000'
+    )
+
+    urlpatterns += [
+        re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    ]
+    
+    # 개발환경에서 정적 파일 서빙
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
