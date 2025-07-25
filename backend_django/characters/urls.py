1c452312d99fb62bc42dee1f42d825592bce4329
daiseek <dnjsgml0578@gmail.com>
2025-07-25 21:48:09 +0900

    fix: 비동기 대본생성 API url 교체

--- backend_django/characters/urls.py
+++ backend_django/characters/urls.py
@@ -8,8 +8,8 @@ from .views import (
 
 urlpatterns = [
     path('books/<int:book_id>/', CharacterConditionalCreateOrListView.as_view()), # 캐릭터 생성 혹은 조회 기능
-    path('<int:character_id>/script/', ScriptGenerateView.as_view()),  # 대본 생성 기능 (동기)
-    path('<int:character_id>/scripts/async/', ScriptGenerateAsyncView.as_view()),  # 대본 생성 기능 (비동기)
+    path('<int:character_id>/script/sync/', ScriptGenerateView.as_view()),  # 대본 생성 기능 (동기)
+    path('<int:character_id>/scripts/', ScriptGenerateAsyncView.as_view()),  # 대본 생성 기능 (비동기)
     path('tasks/<str:task_id>/status/', ScriptTaskStatusView.as_view()),  # 대본 생성 상태 확인 (Redis 기반)
 
 ]
